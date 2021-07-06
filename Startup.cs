f62f8270141aac238deccfc59b87b09c435f8da7
Sergey Petrov <white_duke@mail.ru>
2021-07-06 11:43:29 +0300

    fix: репозиторий(сервис) данных создается в единственном экземпляре

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using PotatoPlace.Models;
 using PotatoPlace.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PotatoPlace
 {
@@ -28,7 +21,7 @@ namespace PotatoPlace
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddTransient<IPotatoService, PotatoService>();
+            services.AddSingleton<IPotatoService, PotatoService>();
             services.AddSwaggerDocument();
         }
 
